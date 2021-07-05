12042ae46d8b50585f524f28099e930b3f442cec
Vinicius Roberto <vinicius.rob2002@gmail.com>
2021-07-05 10:23:54 -0300

    Adicionando componentes de Excluir e Atualizar

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -15,12 +15,28 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
+  //Pesquisar todos os temas
   getAllTema(): Observable<Tema[]>{
     return this.http.get<Tema[]>('https://bpdvrfds.herokuapp.com/temas', this.token)
   }
 
+  //Pesquisar por Id
+  getByIdTema(id: number):Observable<Tema>{
+    return this.http.get<Tema>(`https://bpdvrfds.herokuapp.com/temas/${id}`, this.token)
+  }
+
+  //Inserir um novo tema
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://bpdvrfds.herokuapp.com/temas', tema, this.token)
   }
 
+  //Editar um tema
+  putTema(tema:Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://bpdvrfds.herokuapp.com/temas', tema, this.token)
+  }
+
+  //Excluir um tema
+  deleteTema(id: number){
+    return this.http.delete(`https://bpdvrfds.herokuapp.com/temas/${id}`, this.token)
+  }
 }
